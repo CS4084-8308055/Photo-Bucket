3fec82ccf72e2612cfcc6d015a2a87f361c359c1
David-Beck-UL <39083800+David-Beck-UL@users.noreply.github.com>
2018-11-09 14:43:33 +0000

    Testing Firebase

--- PhotoBucket/app/src/main/java/ie/ul/davidbeck/photobucket/MainActivity.java
+++ PhotoBucket/app/src/main/java/ie/ul/davidbeck/photobucket/MainActivity.java
@@ -1,15 +1,32 @@
 package ie.ul.davidbeck.photobucket;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "PB";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +35,37 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //  Temp testing area
+        // TODO: comment out
+//        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+//
+//        db.collection("photobucket")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (DocumentSnapshot document : task.getResult()) {
+//                                Log.d(TAG, document.getId() + " => " + document.getData());
+//                            }
+//                        } else {
+//                            Log.w(TAG, "Error getting documents.", task.getException());
+//                        }
+//                    }
+//                });
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                // Create a new user with a first and last name
+//                Map<String, Object> pb = new HashMap<>();
+//                pb.put("caption", "caption");
+//                pb.put("imageUrl", "image url");
+//                pb.put("created", new Date());
+
+// Add a new document with a generated ID
+                db.collection("photobucket").add(pb);
+                Snackbar.make(view, "Added a Firestore document", Snackbar.LENGTH_LONG).show();
             }
         });
     }
